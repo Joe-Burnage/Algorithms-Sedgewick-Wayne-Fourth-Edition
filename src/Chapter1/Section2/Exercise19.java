3256d447dc8060e56485c5b1ac8f2081febc8455
Joe Burnage <joeburnage@hotmail.co.uk>
2024-07-23 16:07:22 +0900

    add description and remove Date_v1 dependence

--- src/Chapter1/Section2/Exercise19.java
+++ src/Chapter1/Section2/Exercise19.java
@@ -1,7 +1,18 @@
 package Chapter1.Section2;
 
 import edu.princeton.cs.algs4.StdOut;
+/*
+Parsing. Develop the parse constructors for your Date and Transaction implementations
+of Exercise 1.2.13 that take a single String argument to specify the
+initialization values, using the formats given in the table below.
 
+type                      format                          example
+
+Date           integers separated by slashes             5/22/1939
+
+Transaction     customer, date, and amount,
+                  separated by whitespace         Turing 5/22/1939 11.99
+*/
 
 public class Exercise19
 {
@@ -23,20 +34,18 @@ class Date_v2 extends Date_v1
     int m = Integer.parseInt(fields[1]);
     int d = Integer.parseInt(fields[2]);
     super(y, m, d);
-
   }
 }
 
 class Transaction_v2 extends Transaction_v1
 {
+
   public Transaction_v2(String transaction)
   {
     String[] fields = transaction.split(" ");
     if (fields.length != 3) throw new RuntimeException("Please enter transaction in the form NAME YYYY/MM/DD AMOUNT");
     String who = fields[0];
-    Date_v2 temp = new Date_v2(fields[1]);
-    //super class in Exercise13 requires Date_v1.Date object
-    Date_v1 when = new Date_v1(temp.getYear(), temp.getMonth(), temp.getDay());
+    Date_v2 when = new Date_v2(fields[1]);
     double amount = Double.parseDouble(fields[2]);
     super(who, when, amount);
   }
